eaf4b8966c993d1db09db0ecfc0bd2b186363658
manojsdeveloper <manoj.antony32@gmail.com>
2021-10-24 21:12:42 +0530

    * CamundaCloud.env: * Cloudstarter.csproj: * ZeebeController.cs:
    * Startup.cs: Zeebe environment added

---
+++ Controllers/ZeebeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Cloudstarter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cloudstarter.Controllers
+{
+    public class ZeebeController : Controller
+    {
+        private readonly IZeebeService _zeebeService;
+
+        public ZeebeController(IZeebeService zeebeService)
+        {
+            _zeebeService = zeebeService;
+        }
+
+        [Route("/status")]
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return (await _zeebeService.Status()).ToString();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Cloudstarter.Services;
+using dotenv.net.DependencyInjection.Microsoft;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,12 +29,19 @@ namespace Cloudstarter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<IZeebeService, ZeebeService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cloudstarter", Version = "v1" });
             });
+            services.AddEnv(builder => {
+                builder
+                    .AddEnvFile("CamundaCloud.env")
+                    .AddThrowOnError(false)
+                    .AddEncoding(Encoding.ASCII);
+            });
+            services.AddEnvReader();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
